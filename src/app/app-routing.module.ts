462ae9369db9431e43255159f718ab256a1722c9
Garavirod <rodrigogarciaavila26@gmail.com>
2021-02-10 16:29:40 -0600

    Profile view was done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CredentialsComponent } from './views/credentials/credentials.component';
+import { ProfileComponent } from './views/profile/profile.component';
 
 const routes: Routes = [
   {path: '', component: CredentialsComponent},
+  {path: 'profile', component: ProfileComponent},
   {path: '', pathMatch: 'full', redirectTo: '' },
   {path: '**', pathMatch: 'full', redirectTo: '' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { CredentialsComponent } from './views/credentials/credentials.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './views/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    CredentialsComponent
+    CredentialsComponent,
+    NavbarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
