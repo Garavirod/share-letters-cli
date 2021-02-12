1c7b07b31a6df2e99f441bbe7abe672f3be80ad0
Garavirod <rodrigogarciaavila26@gmail.com>
2021-02-12 14:08:51 -0600

    writting from was validdated and profile form was modified

--- src/app/views/writtingview/writtingview.component.ts
+++ src/app/views/writtingview/writtingview.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-writtingview',
   templateUrl: './writtingview.component.html',
   styleUrls: ['./writtingview.component.css']
 })
 export class WrittingviewComponent implements OnInit {
+  historiaModel = {
+    titulo: '',
+    narrativa: '',
+    genero:'',
+    sinopsis:'',
+    status:false,
+    contenido:'',
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   recoverContent(content:string){
-    console.log("Story content >", content);
+    this.historiaModel.contenido = content;
+  }
+
+  public sendStory(formRef: NgForm){
+    this.historiaModel = {...this.historiaModel, ...formRef.value}
+    console.log(this.historiaModel);
     
   }
 }
