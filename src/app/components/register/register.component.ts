e002031f1bf1af69b1dd6e64e8155604e4217220
Garavirod <rodrigogarciaavila26@gmail.com>
2021-02-11 20:59:40 -0600

    register form was validated

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  userRegister = {
+    username:'',
+    email:'',
+    password:'',
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  registerUser(formRef: NgForm):void{
+    console.log(formRef.value);
+    
+  }
 }
