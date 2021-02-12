53a4ed1f4c9d7086184ad1dff056d36244dbc11f
Garavirod <rodrigogarciaavila26@gmail.com>
2021-02-12 10:57:43 -0600

    new story form was validated

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { HistoriasService } from 'src/app/services/historias.service';
 
 @Component({
@@ -8,10 +9,25 @@ import { HistoriasService } from 'src/app/services/historias.service';
 })
 export class ProfileComponent implements OnInit {
   historias: Array<any> = [];
+  //Model story
+  story={
+    titulo:"",
+    narrativa:"",
+    genero:"",
+    autor:"",
+    valoracion:0,
+    sinopsis:"",
+    contenido:"",
+    status:false,
+    
+  }
   constructor(private historiasService: HistoriasService) { };
 
   ngOnInit(): void {
     this.historias =  this.historiasService.getHistorias();
   }
 
+  createStory(formRef: NgForm):void{
+    console.log(formRef.value);    
+  }
 }
