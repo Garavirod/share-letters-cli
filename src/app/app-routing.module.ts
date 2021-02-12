1378c886fc26d8942244202c0e2c6416800bf571
Garavirod <rodrigogarciaavila26@gmail.com>
2021-02-12 15:37:47 -0600

    guard was implememted for protecting routes and from comentary was validadted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { BusquedaviewComponent } from './views/busquedaview/busquedaview.component';
 import { CredentialsComponent } from './views/credentials/credentials.component';
 import { EscritorviewComponent } from './views/escritorview/escritorview.component';
@@ -9,11 +10,11 @@ import { WrittingviewComponent } from './views/writtingview/writtingview.compone
 
 const routes: Routes = [
   {path: '', component: CredentialsComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'writting/:id', component: WrittingviewComponent},
-  {path: 'reading/:id', component: ReadingviewComponent},
-  {path: 'busqueda', component: BusquedaviewComponent},
-  {path: 'escritor/:id', component: EscritorviewComponent},
+  {path: 'profile', component: ProfileComponent, canActivate:[AuthGuard] },
+  {path: 'writting/:id', component: WrittingviewComponent, canActivate:[AuthGuard]},
+  {path: 'reading/:id', component: ReadingviewComponent, canActivate:[AuthGuard]},
+  {path: 'busqueda', component: BusquedaviewComponent, canActivate:[AuthGuard]},
+  {path: 'escritor/:id', component: EscritorviewComponent, canActivate:[AuthGuard]},
   {path: '', pathMatch: 'full', redirectTo: '' },
   {path: '**', pathMatch: 'full', redirectTo: '' }
 ];
